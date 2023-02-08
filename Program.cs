7349cbabbc37bd02c7787b5a503800cf39f8306b
sumitradas94 <sumitradas94@gmail.com>
2023-02-08 18:59:16 +0530

    Greetings

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollServiceSQL
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Employee Payroll Problem Using MultiThreading.*****");
+        }
+    }
+}
