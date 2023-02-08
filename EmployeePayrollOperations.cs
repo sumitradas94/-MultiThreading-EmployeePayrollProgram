e9f4b7aeefcd1ef80fbc51a12bc3dfc47fe1f2a7
sumitradas94 <sumitradas94@gmail.com>
2023-02-09 04:46:52 +0530

    adding employeeToPayroll method without thread

---
+++ EmployeePayrollOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace EmployeePayrollServiceSQL
+{
+    public class EmployeePayrollOperations
+    {
+        List<EmployeeDetails> employeePayrollDetailList = new List<EmployeeDetails>();
+
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeePayrollDataList)
+        {
+            employeePayrollDataList.ForEach(employeeData =>
+            {
+
+                Console.WriteLine("Employee being added: " + employeeData.EmployeeName);
+                this.addEmployeePayroll(employeeData);
+                Console.WriteLine("Employee Added: " + employeeData.EmployeeName);
+            });
+
+            Console.WriteLine(this.employeePayrollDetailList.ToString());
+        }
+
+        public void addEmployeePayroll(EmployeeDetails emp)
+        {
+            employeePayrollDetailList.Add(emp);
+        }
+
+    }
+}
